c624dd3a6bd9ea4ee0e32aedfa9829cc366ede4f
Elvis Farias <elvisrfarias1@gmail.com>
2023-04-23 13:45:36 -0300

    implantado o delete

--- src/paginas/Administracao/Restaurantes/AdministracaRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaRestaurantes.tsx
@@ -1,48 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import IRestaurante from '../../../interfaces/IRestaurante';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { CreateOutlined } from '@mui/icons-material';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import IRestaurante from "../../../interfaces/IRestaurante"
 
-const AdministracaRestaurantes = () => {
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+const AdministracaoRestaurantes = () => {
 
-  useEffect(() => {
-    axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
-      .then((response) => {
-        setRestaurantes(response.data)
-      }).catch(error => console.log(error))
-  })
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
 
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Restaurantes</TableCell>
-              <TableCell>Editar</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {restaurantes.map((restaurante) =>
-              <TableRow key={restaurante.id}>
-                <TableCell>
-                  {restaurante.nome}</TableCell>
-                <TableCell>
-                  <Link to={`/admin/restaurantes/${restaurante.id}`}>
-                    <CreateOutlined color='primary' />
-                  </Link>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  )
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
+    }, [])
+
+    const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+            .then(() => {
+                const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
+                setRestaurantes([ ...listaRestaurante ])
+            })
+    }
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                        <TableCell>
+                            {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link> ]
+                        </TableCell>
+                        <TableCell>
+                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                Excluir
+                            </Button>
+                        </TableCell>
+                    </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
 }
 
-export default AdministracaRestaurantes;
+export default AdministracaoRestaurantes
